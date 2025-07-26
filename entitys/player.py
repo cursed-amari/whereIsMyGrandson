55b7261a490dfdaa421e973cdecd6765a3a53581
Your Name <you@example.com>
2025-07-27 00:28:08 +0300

    fix add super().__init__()

--- entitys/player.py
+++ entitys/player.py
@@ -13,6 +13,7 @@ class Player(Entity):
     state = State.idle
 
     def __init__(self, pos: Vector2, frames: list[Surface]):
+        super().__init__()
         self.add_component("transform", Transform(pos))
         self.add_component("movement", Movement(PLAYER_SPEED))
         self.add_component("animation", Animation(frames))
