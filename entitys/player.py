665b165bee2c76be0bbf0f963e1d45305df107c8
Your Name <you@example.com>
2025-07-30 16:57:31 +0300

    refactor player

--- entitys/player.py
+++ entitys/player.py
@@ -1,20 +1,27 @@
 from pygame import Vector2
 
 from components.animation_state import AnimationState
+from components.animation import Animation
 from components.direction import Direction
 from components.health import Health
 from components.movement import Movement
 from components.transform import Transform
 from core.settings import PLAYER_SPEED, PLAYER_HP
 from entitys.entity import Entity
+from service.AnimationResource import AnimationResource
+
+
+START_ANIMATION = "idle"
 
 
 class Player(Entity):
     def __init__(self, pos: Vector2):
         super().__init__()
         self.entity_type = "player"
+        
         self.add_component("transform", Transform(pos))
         self.add_component("movement", Movement(PLAYER_SPEED))
         self.add_component("direction", Direction())
         self.add_component("health", Health(PLAYER_HP))
-        self.add_component("state", AnimationState(self.entity_type, "idle"))
+        self.add_component("state", AnimationState(self.entity_type, START_ANIMATION))
+        self.add_component("animation", Animation(AnimationResource().get_animation_frames(self.entity_type, START_ANIMATION)))
